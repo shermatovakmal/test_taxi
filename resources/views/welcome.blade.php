7769fe8d560d06cadfcc743a03175e69a6d74f55
Akmal Shermatov <shermatovakmal@gmail.com>
2022-08-18 11:29:35 +0500

    added-swagger and authentication swagger documentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,6 @@
 
 @section('main')
     <div class="alert alert-info">Задача 1. Автоназначение. <a href="{{ route('orders.list') }}">Перейти</a></div>
-    <div class="alert alert-info">Задача 2. API-для регистрации и авторизации пользователя. <a href="">Перейти</a></div>
+    <div class="alert alert-info">Задача 2. API-для регистрации и авторизации пользователя. <a target="_blank" href="{{url('/api/documentation')}}">Перейти</a></div>
     <div class="alert alert-info">Задача 3. Биллинг. <a href="{{ route('orders.ongoing') }}">Перейти</a></div>
 @endsection
